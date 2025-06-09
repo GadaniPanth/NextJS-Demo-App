7d9457435de513987345c9b05c9400a7c1506a58
GadaniPanth <gadanipanth@gmail.com>
2025-06-09 16:06:47 +0530

    fixed something

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,30 +1,29 @@
+// app/blog/[id]/page.tsx
+
 import Link from "next/link";
 
 async function getPost(id: string) {
   const res = await fetch(`https://dummyjson.com/posts/${id}`);
-  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  if (!res.ok) throw new Error("Post not found");
   return res.json();
 }
 
-interface PostParams {
+interface PageProps {
   params: {
     id: string;
   };
 }
 
-export default async function PostPage({ params }: PostParams) {
-  const postId = params.id;
-  const post = await getPost(postId);
+export default async function PostPage({ params }: PageProps) {
+  const post = await getPost(params.id);
 
   return (
-    <>
-      <div className="wrapper">
-        <article>
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
-          <Link href="/">Back to home</Link>
-        </article>
-      </div>
-    </>
+    <div className="wrapper">
+      <article>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+        <Link href="/">Back to home</Link>
+      </article>
+    </div>
   );
 }
